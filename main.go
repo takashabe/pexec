54e24d8093831fe5d3e936045ac4c226fc76f738
takashabe <takashabe.io@gmail.com>
2019-04-09 16:32:52 +0900

    Added spf13/cobra

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"sync"
+
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -15,11 +16,25 @@ var (
 )
 
 func main() {
-	flag.StringVar(&command, "e", "", "require: the exec command.")
-	flag.IntVar(&count, "c", 1, "optional: number of executions. default 1.")
-	flag.IntVar(&parallel, "p", 1, "optional: number of parallel executions. default 1(not parallels).")
-	flag.Parse()
+	NewCommand().Execute()
+}
 
+func NewCommand() *cobra.Command {
+	root := &cobra.Command{
+		Use:   "pexec",
+		Short: "Executions any command",
+		Run:   execution,
+	}
+
+	root.PersistentFlags().StringVarP(&command, "exec", "e", "", "Exec command (required)")
+	root.PersistentFlags().IntVarP(&count, "count", "c", 1, "Number of executions")
+	root.PersistentFlags().IntVarP(&parallel, "parallel", "p", 1, "Number of parallel executions")
+	root.MarkPersistentFlagRequired("exec")
+
+	return root
+}
+
+func execution(cmd *cobra.Command, args []string) {
 	semaphore := make(chan struct{}, parallel)
 	wg := sync.WaitGroup{}
 	wg.Add(count)
@@ -31,10 +46,10 @@ func main() {
 				wg.Done()
 			}()
 
-			cmd := exec.Command("sh", "-c", command)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Run(); err != nil {
+			ec := exec.Command("sh", "-c", command)
+			ec.Stdout = os.Stdout
+			ec.Stderr = os.Stderr
+			if err := ec.Run(); err != nil {
 				fmt.Printf("received error: %s, command: %s", err.Error(), command)
 			}
 		}()
